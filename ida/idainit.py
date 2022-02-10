db5bbd6b555da1f576c5d4d0376084831a18cd8d
htnhan <huynh.t.nhan@gmail.com>
2022-02-10 10:57:56 -0600

    print and string stuff

--- ida/idainit.py
+++ ida/idainit.py
@@ -8,12 +8,12 @@ def hightlight_jmpregs():
     COLOR = 0x000077
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
     for ea in heads:
-        if not idc.print_insn_mnem(ea) == 'jmp':
+        if not idc.print_insn_mnem(ea) == "jmp":
             continue
         if idc.get_operand_type(ea, 0) == OPTYPE_REGISTER:
             idc.set_color(ea, CIC_ITEM, COLOR)
-            print 'Call to register at 0x%08x: %s %s' % (
-                    ea, idc.print_insn_mnem(ea), idc.print_operand(ea, 0))
+            print ("Call to register at 0x%08x: %s %s" % (
+                    ea, idc.print_insn_mnem(ea), idc.print_operand(ea, 0)))
     return
 
 
@@ -22,11 +22,11 @@ def hightlight_antivms():
     most benign code'''
     COLOR = 0x000077
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
-    antivm_instructions = set(['sidt', 'sgdt', 'sldt',
-                               'smsw', 'str', 'in', 'cpuid'])
+    antivm_instructions = set(["sidt", "sgdt", "sldt",
+                               "smsw", "str", "in", "cpuid"])
     for ea in heads:
         if idc.print_insn_mnem(ea).lower() in antivm_instructions:
-            print "Detect anti-vm at 0x%08x" % (ea)
+            print ("Detect anti-vm at 0x%08x" % (ea))
             idc.set_color(ea, CIC_ITEM, COLOR)
     return 0
 
@@ -35,9 +35,9 @@ def hightlight_calls():
     '''Hightlight all calls, with dark background'''
     COLOR = 0x000077
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
-    print 'hightlight calls called'
+    print ("hightlight calls called")
     for h in heads:
-        if idc.print_insn_mnem(h) == 'call':
+        if idc.print_insn_mnem(h) == "call":
             idc.set_color(h, CIC_ITEM, COLOR)
 
     return 0
@@ -50,12 +50,12 @@ def hightlight_peb():
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
     for ea in heads:
         op0, op1 = idc.print_operand(ea, 0), idc.print_operand(ea, 1)
-        if 'fs:' in op0.lower() or 'fs:' in op1.lower():
+        if "fs:" in op0.lower() or "fs:" in op1.lower():
             idc.set_color(ea, CIC_ITEM, COLOR)
-            print 'Accessing fs at 0x%x' % (ea,)
-        elif 'gs:' in op0.lower() or 'gs:' in op1.lower():
+            print ("Accessing fs at 0x%x" % (ea,))
+        elif "gs:" in op0.lower() or "gs:" in op1.lower():
             idc.set_color(ea, CIC_ITEM, COLOR)
-            print 'Accessing gs at 0x%x' % (ea,)
+            print ("Accessing gs at 0x%x" % (ea,))
     return
 
 
@@ -64,9 +64,9 @@ def hightlight_xor():
     '''
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
     COLOR = 0x005757
-    print 'hightlight calls called'
+    print "hightlight calls called"
     for h in heads:
-        if idc.print_insn_mnem(h) == 'xor':
+        if idc.print_insn_mnem(h) == "xor":
             idc.set_color(h, CIC_ITEM, COLOR)
 
     return 0
@@ -75,10 +75,10 @@ def hightlight_xor():
 def reset_color():
     '''reset all colors'''
     heads = Heads(idc.get_segm_start(idc.get_screen_ea()), idc.get_segm_end(idc.get_screen_ea()))
-    print 'hightlight calls called'
+    print ("hightlight calls called")
     COLOR = 0x0
     for h in heads:
-        if idc.print_insn_mnem(h) == 'xor':
+        if idc.print_insn_mnem(h) == "xor":
             idc.set_color(h, CIC_ITEM, COLOR)
 
     return 0
@@ -105,25 +105,25 @@ def mkvtable(name, vtblref, count):
         addrsize = 4
 
     msid = idc.add_struc(-1, name, 0)
-    print 'added', hex(msid), type(msid)
+    print ("added", hex(msid), type(msid))
     if msid == 0xFFFFFFFF:
-        print 'failed'
+        print "failed"
         return
 
     for _ in xrange(count):
         offset = _ * addrsize
         addr = hex(fn(vtblref + offset))[:-1]
-        membername = 'field_%x' % (offset,)
-        print 'adding member:', membername,
+        membername = "field_%x" % (offset,)
+        print ("adding member:", membername)
         rc = idc.add_struc_member(msid, membername, -1, FLAGS, -1, addrsize)
-        print hex(rc)
+        print (hex(rc))
         idc.set_member_cmt(msid, offset, addr, 1)
 
 
 def help():
-    print 'reset_color()'
-    print 'nopit(start, end)'
-    print 'mkvtable(name, vtblref, count)'
+    print "reset_color()"
+    print "nopit(start, end)"
+    print "mkvtable(name, vtblref, count)"
 
 def main():
     reset_color()
@@ -134,7 +134,7 @@ def main():
     hightlight_jmpregs()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 
